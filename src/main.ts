b06c213544736acb09545ed1a01aff8250e9f882
decohachi <decohachi@decohachi.local>
2020-08-10 17:32:03 +0900

    Pixi.jsでHello World

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import * as PIXI from 'pixi.js';
+const app = new PIXI.Application({backgroundColor: 0xffffff});
+document.body.appendChild(app.view);
+
+const txt = new PIXI.Text('Hello World')
+app.stage.addChild(txt)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+  devtool: "inline-source-map",
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: "./src/main.ts",
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: "ts-loader"
+      }
+    ]
+  },
+  // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる。
+  resolve: {
+    extensions: [".js", '.ts']
+  },
+};
