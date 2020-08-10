f1ee36753ebbedac65bd0132781b95a2cd147ee3
decohachi <decohachi@decohachi.local>
2020-08-10 19:42:45 +0900

    Prettierの導入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import * as PIXI from 'pixi.js';
-const app = new PIXI.Application({backgroundColor: 0xffffff});
+import * as PIXI from "pixi.js";
+const app = new PIXI.Application({ backgroundColor: 0xffffff });
 document.body.appendChild(app.view);
 
-const txt = new PIXI.Text('Hello World')
-app.stage.addChild(txt)
+const txt = new PIXI.Text("Hello World");
+app.stage.addChild(txt);
